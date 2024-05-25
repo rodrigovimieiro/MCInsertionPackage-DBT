324373ecc3db9df3ba8b9909f3f80b49c98efac2
rodrigovimieiro <rodrigo.vimieiro@gmail.com>
2024-05-25 10:41:28 -0300

    4AFC JND on Hologic Several modifications to make it work on Hologic 3D for a 4AFC test

--- tools/recon_simu_mcs_projs.py
+++ tools/recon_simu_mcs_projs.py
@@ -10,6 +10,7 @@ import sys
 import numpy as np
 import pydicom
 import pathlib
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from scipy.io import loadmat
@@ -33,10 +34,10 @@ if __name__ == '__main__':
     pathBuildDirpyDBT           = '/home/rodrigo/Documents/Rodrigo/Codigos/pyDBT/build'
     pathPatientDensity          = pathPatientCases + '/density'
     pathPatientCalcs            = pathPatientCases + '/calcifications'
-    
-    cluster_dimensions  = (20, 20, 10)              # In mm
-    
-    contrasts = [0.3]
+
+    contrasts = [0.2]
+    for x in range(14):
+        contrasts.append(np.round(0.85 * contrasts[x], 3))
     
     # List all patients    
     patient_cases = [str(item) for item in pathlib.Path(pathPatientCalcs).glob("*") if pathlib.Path(item).is_dir()]
@@ -47,37 +48,65 @@ if __name__ == '__main__':
     
     # Create a DBT geometry  
     geo = geometry_settings()
-    geo.GE()
-    
+    geo.Hologic()
+
+    file = pathlib.Path('../data/IDs_processed_recon.txt')
+    if file.exists():
+        with file.open('r') as file:
+            lines = file.readlines()
+        ids_processed = [line.strip() for line in lines]
+    else:
+        ids_processed = []
+
+    file = pathlib.Path('../data/to_exclude.txt')
+    if file.exists():
+        with file.open('r') as file:
+            lines = file.readlines()
+        to_exclude = [line.strip() for line in lines]
+    else:
+        to_exclude = []
+
+    df = pd.read_csv('../data/status.csv')
+    # Filter cases that have ROIs
+    df = df[~((df['nROIsNoCluster'] == 0) & (df['nROIsCluster'] == 0))]
+
     for patient_case in patient_cases:
         
-        exams = [str(item) for item in pathlib.Path(patient_case, 'DBT').glob("*") if pathlib.Path(item).is_dir()]
+        exams = [str(item) for item in pathlib.Path(patient_case).glob("*") if pathlib.Path(item).is_dir()]
         
-        for exam in exams: 
-            
-            path2write_patient_name = "{}{}{}".format(pathPatientDensity , filesep(), "/".join(exam.split('/')[-3:]))
-            
-            res = loadmat('{}{}Result_Images{}Masks_{}.mat'.format(path2write_patient_name, filesep(), filesep(), 4))['res']
-            
-            mask_breast = res['BreastMask'][0][0]
-                        
-            bdyThick = np.load('{}{}Result_Images{}bodyPartThickness.npy'.format(path2write_patient_name, filesep(), filesep()))   
-            
-            path2write_patient_name = "{}{}{}".format(pathPatientCalcs , filesep(), "/".join(exam.split('/')[-3:]))
-            
-            flags =  np.load(path2write_patient_name + '{}flags.npy'.format(filesep()), allow_pickle=True)[()]
-            
-            if not flags['right_breast']:
-                mask_breast = np.fliplr(mask_breast)
+        for exam in exams:
 
-            bound_X = np.max((int(np.where(np.sum(mask_breast, axis=0) > 1)[0][0]) - 30, 0))
+            current_id = '/'.join(exam.split('/')[-2:])
 
-            for contrast in contrasts:
-                
+            # if current_id != '29680212/2105':
+            #     continue
+
+            # ID read not in ID to run
+            if current_id not in df['ID'].values or current_id in ids_processed or current_id in to_exclude:
+                continue
+
+            print("Processing exam: " + current_id)
+
+            with open('../data/IDs_processed_recon.txt', 'a') as file:
+                file.write(current_id + '\n')
+
+            path2write_patient_name = "{}{}{}".format(pathPatientCalcs , filesep(), "/".join(exam.split('/')[-2:]))
+
+            flags = np.load(path2write_patient_name + '{}flags.npy'.format(filesep()), allow_pickle=True)[()]
+
+            bdyThick = flags['bdyThick']
+
+            nROIsNoCluster = df[df.ID == current_id]['nROIsNoCluster'].iloc[0]
+            nROIsCluster = df[df.ID == current_id]['nROIsCluster'].iloc[0]
+
+            for idc, contrast in enumerate(contrasts):
+
+                if idc != 0 and nROIsCluster == 0:
+                    break
+
+                bound_X = flags['bound_X']#np.max((int(np.where(np.sum(mask_breast, axis=0) > 1)[0][0]) - 30, 0))
+
                 path2write_contrast = "{}{}recon_contrast_{:.3f}_ROI".format(path2write_patient_name , filesep(), contrast)
-                
-                if makedir(path2write_contrast):
-                    continue  
                 
                 path2write_contrast = "{}{}contrast_{:.3f}".format(path2write_patient_name , filesep(), contrast)
     
@@ -89,7 +118,7 @@ if __name__ == '__main__':
                     
                     dcmH = pydicom.dcmread(str(dcmFile), force=True)
                     
-                    ind = int(str(dcmFile).split('/')[-1].split('_')[1]) - 1
+                    ind = int(str(dcmFile).split('/')[-1].split('_')[-1].split('.')[0])
                     
                     dcmData[ind] = dcmH.pixel_array.astype('float32').copy()
                  
@@ -112,9 +141,14 @@ if __name__ == '__main__':
                 geo.nu = dcmData.shape[1]      # number of pixels (columns)
                 geo.nv = dcmData.shape[0]      # number of pixels (rows)
                 geo.nz = np.ceil(bdyThick/geo.dz).astype(int)
+
+                geo.dy = 0.14
+                geo.dx = 0.14
                 
-                dcmData = dataPreProcess(dcmData, geo,  flagCropProj=False)
-                
+                # dcmData, _ = dataPreProcess(dcmData, geo,  flagCropProj=False)
+
+                geo.detAngle = 0
+
                 vol = backprojectionDDb_cuda(np.float64(dcmData), geo, -1, libFiles)
                 
                 vol[vol < 0 ] = 0
@@ -125,39 +159,39 @@ if __name__ == '__main__':
                     
                 
                 # The cluster origin is located at the same as the DBT systemes, i.e., right midle. Z is at the half
-                cluster_pixel_size = int(20/0.1)
-                
-                ind_x = ((flags['calc_coords'][0] + flags['bound_X']) - bound_X) - cluster_pixel_size
-                ind_y = flags['calc_coords'][1] - (cluster_pixel_size // 2)
-                ind_z = flags['calc_coords'][2]
-                # plt.imshow(vol[ind_y:ind_y+cluster_pixel_size,
-                #                 ind_x:ind_x+cluster_pixel_size,
-                #                 flags['calc_coords'][2]], 'gray')
-                
-                path2write_contrast = "{}{}recon_contrast_{:.3f}_ROI".format(path2write_patient_name , filesep(), contrast)
-                
-                for z in range(-7,8):
-                    
-                    dcmFile_tmp = path2write_contrast + '{}{}.dcm'.format(filesep(), ind_z + z)
-                    
-                    writeDicom(dcmFile_tmp, np.uint16(vol[ind_y:ind_y+cluster_pixel_size, 
-                                                          ind_x:ind_x+cluster_pixel_size,
-                                                          ind_z + z]))
-                    
-                # if not flags['right_breast']:
-                #     vol = np.fliplr(vol)
-                    
-                
-                # path2write_contrast = "{}{}recon_contrast_{:.3f}".format(path2write_patient_name , filesep(), contrast)
-                
-                # makedir(path2write_contrast)
-                
-                # for z in range(vol.shape[-1]):
-                    
-                #     dcmFile_tmp = path2write_contrast + '{}{}.dcm'.format(filesep(), z)
-                    
-                #     writeDicom(dcmFile_tmp, np.uint16(vol[:,:,z]))
-                    
-                    
+                cluster_pixel_size = int(20/0.140)
+
+                (x_clust, y_clust, z_clust) = flags['calc_coords']
+
+                cluster_flag = flags['cluster_flag']
+
+                for idr in range(len(x_clust)):
+
+                    ind_x = int(x_clust[idr] - (cluster_pixel_size / 2))
+                    ind_y = int(y_clust[idr] - (cluster_pixel_size / 2))
+                    ind_z = z_clust[idr]
+                    # plt.imshow(vol[ind_y:ind_y+cluster_pixel_size,
+                    #                 ind_x:ind_x+cluster_pixel_size,
+                    #                 ind_z], 'gray')
+                    # plt.show()
+
+                    if cluster_flag[idr] == 0 and idc > 0:
+                        continue
+
+                    path2write_contrast = "{}{}recon_roi_{:02d}_contrast_{:.3f}_ROI_{}".format(path2write_patient_name ,
+                                                                                               filesep(),
+                                                                                               idr,
+                                                                                               contrast,
+                                                                                               'cluster1' if cluster_flag[idr] else 'cluster0')
+
+                    makedir(path2write_contrast)
+
+                    for z in range(-7, 8):
+
+                        dcmFile_tmp = path2write_contrast + '{}{}.dcm'.format(filesep(), ind_z + z)
+
+                        writeDicom(dcmFile_tmp, np.uint16(vol[ind_y:ind_y+cluster_pixel_size,
+                                                              ind_x:ind_x+cluster_pixel_size,
+                                                              ind_z + z]))
                 
                 
